63d427f63bc70096961788027a34576d4f99ec09
klimh <klimgudzenko@gmail.com>
2026-04-04 14:29:36 +0200

    uzupełnienie kodu: działa dodanie usera i zabezpieczenie przy dodaniu tego samego maila

--- backend/api/user.py
+++ backend/api/user.py
@@ -8,5 +8,22 @@ from backend.schemas.user import UserCreate, UserResponse
 router = APIRouter(prefix = "/users", tags = ["Użytkownicy"])
 
 @router.post("/", response_model = UserResponse)
-def create_user():
-    pass
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    db_user = db.query(User).filter(User.email == user.email).first()
+    if db_user:
+        raise HTTPException(status_code=400, detail="Ten email został już przypisany do istniejącego konta")
+
+    new_user = User(
+        first_name = user.first_name,
+        last_name = user.last_name,
+        email=user.email,
+        password=user.password,
+        role=user.role
+    )
+
+    #tu zapisujemy w bazie
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
-from db.database import engine, Base
+from backend.db.database import engine, Base
 
-from db_models import user
-from api import user as user_api
+from backend.db_models import user
+from backend.api import user as user_api
 
 Base.metadata.create_all(bind=engine)
 app = FastAPI(title = "RehabSense API", version = "1.6")
