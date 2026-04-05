67b9b385fd0ec77d83964a745b7b6f333b1166aa
klimh <klimgudzenko@gmail.com>
2026-04-05 10:44:27 +0200

    konfiguracja JWT i schemat tokenu

--- backend/core/security.py
+++ backend/core/security.py
@@ -1,4 +1,14 @@
+import os
+from datetime import datetime, timedelta, timezone
 from passlib.context import CryptContext
+import jwt
+from dotenv import load_dotenv, dotenv_values
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM", "HS256")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
 
 pwd_context = CryptContext(schemes=["bcrypt"],deprecated="auto")
 
@@ -9,3 +19,11 @@ def get_password_hash(password: str) -> str:
 def verify_password(tested_password: str, hashed_password: str) -> bool:
     """sprawdza, czy podane haslo pasuje do hasha z bazy"""
     return pwd_context.verify(tested_password,hashed_password)
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
