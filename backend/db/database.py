24e9352e6d700323470a8106aef5f414b80248cb
Remko23 <r-tomecki.live@wp.pl>
2026-06-08 10:59:29 +0200

    fix: first login network error

--- backend/db/database.py
+++ backend/db/database.py
@@ -9,7 +9,7 @@ SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./rehabsense.db")
 
 connect_args = {"check_same_thread": False} if SQLALCHEMY_DATABASE_URL.startswith("sqlite") else {}
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args=connect_args)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args=connect_args, pool_pre_ping=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
